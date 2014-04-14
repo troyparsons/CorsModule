27f6398f278c63571984950a072d89c9484ec377
troyparsons <troy@troyparsons.com>
2014-04-14 13:40:12 +1000

    Tie in code analysis settings & custom dictionary

--- src/assemblies/CorsModule/OriginCollection.cs
+++ src/assemblies/CorsModule/OriginCollection.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.Web.Administration;
+﻿using System.Collections;
+using Microsoft.Web.Administration;
 
 namespace Cors
 {
     public class OriginCollection : ConfigurationElementCollectionBase<OriginConfigurationElement>
     {
+        public void CopyTo(OriginConfigurationElement[] array, int index)
+        {
+            ((ICollection) this).CopyTo(array, index);
+        }
     }
 }
--- src/assemblies/CorsModule/ResourceCollection.cs
+++ src/assemblies/CorsModule/ResourceCollection.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.Web.Administration;
+﻿using System.Collections;
+using Microsoft.Web.Administration;
 
 namespace Cors
 {
     public class ResourceCollection : ConfigurationElementCollectionBase<ResourceConfigurationElement>
     {
+        public void CopyTo(ResourceConfigurationElement[] array, int index)
+        {
+            ((ICollection)this).CopyTo(array, index);
+        }
     }
 }
